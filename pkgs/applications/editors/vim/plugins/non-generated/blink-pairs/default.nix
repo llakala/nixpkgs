f12bab67031dfd290251df0923f2a6021af32a4a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-03-26 15:53:31 -0400

    vimPlugins.blink-pairs: 0.4.1 -> 0.5.0

--- pkgs/applications/editors/vim/plugins/non-generated/blink-pairs/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/blink-pairs/default.nix
@@ -8,13 +8,13 @@
   nix-update-script,
 }:
 let
-  version = "0.4.1";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "Saghen";
     repo = "blink.pairs";
     tag = "v${version}";
-    hash = "sha256-IfnFSusQMm6LujE1AmihK9wEF2RSGfKYwpV2fedg0fc=";
+    hash = "sha256-PTbj6jlXNRUOmwFSplvRDDiyyGqkBzUKtuBrvZm9kzM=";
   };
 
   blink-pairs-lib = rustPlatform.buildRustPackage {
