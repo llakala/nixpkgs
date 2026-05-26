0609f912bb4e1bcca976b3d72fd8793226f5644d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-05-26 13:23:08 -0400

    lib.types: remove assertMsg usage
    assertMsg sends our error message through a function call, even if the
    error condition doesn't trigger. This requires a lot of thunk allocation
    that can be easily avoided.

--- lib/types.nix
+++ lib/types.nix
@@ -452,7 +452,7 @@ rec {
       betweenDesc = lowest: highest: "${toString lowest} and ${toString highest} (both inclusive)";
       between =
         lowest: highest:
-        assert lib.assertMsg (lowest <= highest) "ints.between: lowest must be smaller than highest";
+        assert (lowest <= highest) || throw "ints.between: lowest must be smaller than highest";
         addCheck int (x: x >= lowest && x <= highest)
         // {
           name = "intBetween";
@@ -539,7 +539,7 @@ rec {
     {
       between =
         lowest: highest:
-        assert lib.assertMsg (lowest <= highest) "numbers.between: lowest must be smaller than highest";
+        assert (lowest <= highest) || throw "numbers.between: lowest must be smaller than highest";
         addCheck number (x: x >= lowest && x <= highest)
         // {
           name = "numberBetween";
@@ -1772,9 +1772,9 @@ rec {
   # converted to `finalType` using `coerceFunc`.
   coercedTo =
     coercedType: coerceFunc: finalType:
-    assert lib.assertMsg (
-      coercedType.getSubModules == null
-    ) "coercedTo: coercedType must not have submodules (it’s a ${coercedType.description})";
+    assert
+      (coercedType.getSubModules == null)
+      || throw "coercedTo: coercedType must not have submodules (it’s a ${coercedType.description})";
     mkOptionType rec {
       name = "coercedTo";
       description = "${optionDescriptionPhrase (class: class == "noun") finalType} or ${
